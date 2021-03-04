9b9ee2c0fa7d51f269ca4ab25a1887b46ed56116
John Hauck <jhauck@esri.com>
2021-03-04 16:53:16 -0700

    add license info

--- src/components/solution-variables/solution-variables.tsx
+++ src/components/solution-variables/solution-variables.tsx
@@ -1,4 +1,20 @@
-import { Component, Element, Event, EventEmitter, Host, h, Prop, VNode } from '@stencil/core';
+/** @license
+ * Copyright 2021 Esri
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { Component, Event, EventEmitter, Host, h, Prop, VNode } from '@stencil/core';
 
 export interface IVariableItem {
   id: string;
