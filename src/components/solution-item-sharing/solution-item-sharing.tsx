71564d47c261d9b1becd76f25357369847e96a5c
John Hauck <jhauck@esri.com>
2021-03-10 11:11:06 -0700

    add icons to share component

--- src/components/solution-item-sharing/solution-item-sharing.tsx
+++ src/components/solution-item-sharing/solution-item-sharing.tsx
@@ -20,6 +20,8 @@ export interface IItemShare {
   id: string;
   title: string;
   shareItem: boolean;
+  type: string;
+  typeKeywords: string[];
 }
 
 @Component({
@@ -76,6 +78,7 @@ export class SolutionItemSharing {
             scale="s"
             onCalciteSwitchChange={(event) => this._updateItem(event)}
           ></calcite-switch>
+          <solution-item-icon type={item.type} typeKeywords={item.typeKeywords}></solution-item-icon>
           {item.title}
         </calcite-label>
       );
