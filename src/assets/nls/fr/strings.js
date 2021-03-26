c7959c00ae1d9f08bcff661cfaea8113cf6041fc
Mike Tschudi <mtschudi@esri.com>
2021-03-26 11:03:23 -0700

    Moved strings and styles into global assets

---
+++ src/assets/nls/fr/strings.js
@@ -0,0 +1,70 @@
+﻿/*
+ | Copyright 2021 Esri
+ |
+ | Licensed under the Apache License, Version 2.0 (the "License");
+ | you may not use this file except in compliance with the License.
+ | You may obtain a copy of the License at
+ |
+ |    http://www.apache.org/licenses/LICENSE-2.0
+ |
+ | Unless required by applicable law or agreed to in writing, software
+ | distributed under the License is distributed on an "AS IS" BASIS,
+ | WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ | See the License for the specific language governing permissions and
+ | limitations under the License.
+ */
+/*
+ | Note: this file uses two types of symbols to mark where values are inserted by the program:
+ |   * under the "typescript" section:  ${insert_here}
+ |   * under the "elm" section:  {{insert_here}}
+*/
+define({
+  "configuration_modal": {
+    "title": "Configurer: {{solutionName}}",
+    "cancel": "Annuler",
+    "save": "Enregistrer",
+
+    "configuration": {
+      "definitionTab": "Définition",
+      "spatialReferenceTab": "Référence spatiale",
+
+      "itemDetailsTab": "Détails de l'élément",
+      "dataTab": "Données",
+      "propertiesTab": "Propriétés",
+      "groupDetailsTab": "Détails du groupe",
+      "sharingTab": "Partage",
+
+      "itemDetails": {
+        "editThumbnail": "Mettre à jour la miniature",
+        "description": "Description",
+        "tags": "Balises",
+        "credits": "Crédits (Attribution)",
+        "termsOfUse": "Conditions d’utilisation",
+        "snippetCountPattern": "{{n}} sur 250"
+      },
+
+      "jsonEditor": {
+        "startEditing": "Commencer l'édition",
+        "search": "Chercher",
+        "cancelEdits": "fr-Cancel Edits",
+        "saveEdits": "fr-Save Edits"
+      },
+
+      "resourceItem": {
+        "update": "fr-Update",
+        "download": "fr-Download"
+      },
+
+      "spatialRef": {
+        "specifyParam": "Paramètre de référence spatiale",
+        "defaultSpatialRef": "Référence spatiale par défaut",
+        "featureServicesHeading": "Services de fonctionnalités"
+      },
+
+      "variables": {
+        "orgVariables": "fr-Organization Variables",
+        "solVariables": "fr-Solution Variables"
+      }
+    }
+  }
+});
