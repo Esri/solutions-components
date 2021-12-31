4d83e970dbb32831ebe911df88731feea35447cd
Mike Tschudi <mtschudi@esri.com>
2021-12-31 13:40:01 -0800

    Generalized setLocale so that it can be used in requirejs or dojo environments

--- src/demos/libs/setLocale.js
+++ src/demos/libs/setLocale.js
@@ -1,6 +1,29 @@
 /* Copyright (c) 2021 Environmental Systems Research Institute, Inc.
  * Apache-2.0 */
 
+/*
+  Defines global variables
+    * locale: Locale as defined in URL, local storage, or browser (in that priority order), defaulting to "en";
+          value is coerced to lowercase
+
+  Defines script variables
+    * appLocale: `locale` constrained to the locales supported by this app, defaulting to null
+    * dirNode: The `html` node, in which this script sets the `lang` and `dir` attributes
+    * isRTL: Indicates if the locale represents a RTL locale
+    * lang: Contains the language (initial) part of `locale`, e.g., "fr" for "fr" or "fr-ch" locales
+    * localeConfig: app and monaco editor locale configuration
+    * localeInBrowser: Locale as defined as the first of navigator.languages, navigator.language, or
+    *     naviagor.userLanguage (in that priority order), defaulting to null
+    * localeInLocalStorage: Locale as defined in local storage under the key `locale`, defaulting to null
+    * localeInUrl: locale as defined in URL via the `locale` parameter, defaulting to ""
+    * monacoLocale: `locale` constrained to the locales supported by the monaco editor, defaulting to null
+
+  Defines global functions
+    * getIsRTL: Tests if the two-letter language code represents a RTL language.
+    * getLocaleLang: Returns language part of a locale specification.
+    * getLocaleSupport: Returns highest level of support for a locale: locale and region, locale, or not supported.
+*/
+
 // Get locale sources
 const localeInUrl = ((/[\?&](locale\b=\b[^&]*)/g.exec(window.location.search) || []).pop() || '').split('=').pop();
 const localeInLocalStorage = localStorage.getItem('locale');
@@ -16,33 +39,32 @@ if (localeInUrl) {
 
 const lang = locale.split("-")[0];
 const isRTL = getIsRTL(locale);
-window.localeInUrl = localeInUrl;
 
 // Update the app's page
 const dirNode = document.querySelector('html');
 dirNode.setAttribute('lang', locale);
 dirNode.setAttribute('dir', isRTL ? 'rtl' : 'ltr');
 
-// Test support and initialize requirejs
-const requireConfig = { paths: { vs: '../assets/monaco-editor/min/vs' } };
+// Test support and initialize config file for loader
+const localeConfig = {
+  isRTL,
+  paths: { vs: '../assets/monaco-editor/min/vs' }
+};
 
 const appLocale = getLocaleSupport(locale, '|ar|bg|bs|ca|cs|da|de|el|en|es|et|fi|fr|he|hr|hu|id|it|ja|ko|lt|lv|nb|nl|no|pl|pt-br|pt-pt|ro|ru|sk|sl|sr|sv|th|tr|uk|vi|zh-cn|zh-hk|zh-tw|');
 if (appLocale) {
-  requireConfig.config = { i18n: { locale: appLocale } };
+  localeConfig.config = { i18n: { locale: appLocale } };  // requirejs i18n format
 }
 
 const monacoLocale = getLocaleSupport(locale, '|de|es|fr|it|ja|ko|ru|zh-cn|zh-tw|');
 if (monacoLocale) {
-  requireConfig['vs/nls'] = { availableLanguages: { '*': monacoLocale } };
+  localeConfig['vs/nls'] = { availableLanguages: { '*': monacoLocale } };
 }
-
-requirejs.config(requireConfig);
-
 
 // -------------------------------------------------------------------------------------------------------------------//
 
 /**
- * Tests if the two-letter language code part of a locale is `ar` or `he`.
+ * Tests if the two-letter language code represents a RTL language.
  *
  * @param [string] locale Locale to test
  * @return [boolean] Locale's language is RTL (i.e., `ar` or `he`)
@@ -64,7 +86,8 @@ function getLocaleLang(locale) {
 }
 
 /**
- * Returns highest level of support for a locale: locale and region, locale, or not supported.
+ * Returns highest level of support for a locale: locale and region, locale, or
+ * not supported.
  *
  * @param [string] locale Locale to test
  * @param [string] supportedLocales "|"-separated list of supported locales
