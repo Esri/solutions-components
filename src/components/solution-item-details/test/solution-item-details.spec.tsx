b46809f2d830b81126a7034e6fe409f80d63c003
John Hauck <jhauck@esri.com>
2022-03-22 10:55:13 -0600

    update test

--- src/components/solution-item-details/test/solution-item-details.spec.tsx
+++ src/components/solution-item-details/test/solution-item-details.spec.tsx
@@ -41,7 +41,7 @@ describe('solution-item-details', () => {
 
             <input class="display-none" type="file" accept=".jpg,.gif,.png,image/jpg,image/gif,image/png" />
 
-            <button class="font-size--3 btn-link inline-block">
+            <button class="btn-link font-size--3 inline-block trailer-quarter">
               <svg viewBox="0 0 16 16" width="16" height="16" class="icon-inline icon-inline--on-left">
                 <path d="M14.792 2.666l-1.414-1.413a.965.965 0 0 0-1.385-.03l-1.444 1.444-8.763 8.72L.03 15.481a.371.371 0 0 0 .488.488l4.096-1.756 8.763-8.72-.001-.001.002.002 1.443-1.444a.965.965 0 0 0-.03-1.385zM1.569 14.431l.554-1.293.74.739zm2.338-.924l-1.414-1.414 7.963-7.92 1.414 1.415zm8.67-8.626l-1.413-1.414 1.29-1.29a.306.306 0 0 1 .433 0l.981.98a.306.306 0 0 1 0 .433z"></path>
               </svg> Edit Thumbnail
@@ -105,7 +105,7 @@ describe('solution-item-details', () => {
 
             <input class="display-none" type="file" accept=".jpg,.gif,.png,image/jpg,image/gif,image/png" />
 
-            <button class="font-size--3 btn-link inline-block">
+            <button class="btn-link font-size--3 inline-block trailer-quarter">
               <svg viewBox="0 0 16 16" width="16" height="16" class="icon-inline icon-inline--on-left">
                 <path d="M14.792 2.666l-1.414-1.413a.965.965 0 0 0-1.385-.03l-1.444 1.444-8.763 8.72L.03 15.481a.371.371 0 0 0 .488.488l4.096-1.756 8.763-8.72-.001-.001.002.002 1.443-1.444a.965.965 0 0 0-.03-1.385zM1.569 14.431l.554-1.293.74.739zm2.338-.924l-1.414-1.414 7.963-7.92 1.414 1.415zm8.67-8.626l-1.413-1.414 1.29-1.29a.306.306 0 0 1 .433 0l.981.98a.306.306 0 0 1 0 .433z"></path>
               </svg> Edit Thumbnail
