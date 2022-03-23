0ecc40936c2fb4f92ea78722002d1ee7d34867c5
John Hauck <jhauck@esri.com>
2022-03-23 08:47:10 -0600

    start to add tailwind

---
+++ tailwind.config.ts
@@ -0,0 +1,12 @@
+export default {
+  content: ["./src/components/**/*.scss"],
+  theme: {
+    screens: {
+      s: "480px",
+      m: "864px",
+      l: "1024px",
+      xl: "1440px"
+    }
+  },
+  plugins: []
+};
