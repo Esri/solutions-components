abf952c37706fe582059a553756e6baecad89161
John Hauck <jhauck@esri.com>
2022-08-30 13:42:21 -0600

    add license text

--- src/components/public-notification/public-notification.tsx
+++ src/components/public-notification/public-notification.tsx
@@ -1,3 +1,19 @@
+/** @license
+ * Copyright 2022 Esri
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import { Component, Element, Host, h, Listen, Prop, State, VNode, Watch } from '@stencil/core';
 import { ISelectionSet, EPageType, ESelectionMode, EWorkflowType } from '../../utils/interfaces';
 import { flashSelection, getMapLayerView, highlightFeatures } from '../../utils/mapViewUtils';
@@ -365,7 +381,6 @@ export class PublicNotificationTwo {
         page = (
           <pdf-download
             layerView={this.addresseeLayer}
-            removeDuplicateEnabled={false}
           />
         )
         break;
