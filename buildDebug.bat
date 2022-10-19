105212f1d43dc19542ceb821576adea8d40607fd
Mike Tschudi <mtschudi@esri.com>
2022-10-19 08:54:43 -0700

    Added git info to build

--- buildDebug.bat
+++ buildDebug.bat
@@ -3,3 +3,7 @@ rem Builds the repository
 call npm install
 call npm run build:debug
 
+echo Built %date% %time% >dist\solutions-components_commit.txt
+git rev-parse --abbrev-ref HEAD >>dist\solutions-components_commit.txt
+git log -1>>dist\solutions-components_commit.txt
+
