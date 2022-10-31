5f7244c6888569d37ef61a03b7f9658fd5552504
John Hauck <jhauck@esri.com>
2022-10-31 12:50:04 -0600

    basic check-list test

--- src/components/check-list/test/check-list.spec.tsx
+++ src/components/check-list/test/check-list.spec.tsx
@@ -1,18 +1,35 @@
+import { h } from '@stencil/core';
 import { newSpecPage } from '@stencil/core/testing';
 import { CheckList } from '../check-list';
 
-xdescribe('check-list', () => {
-  it('renders', async () => {
+describe('check-list', () => {
+  it('renders values', async () => {
+    const values = ["A", "B", "C"];
     const page = await newSpecPage({
       components: [CheckList],
-      html: `<check-list></check-list>`,
+      template: () => (<check-list values={values}></check-list>),
     });
+    // TODO understand why the checkboxes don't show the checked prop??
     expect(page.root).toEqualHtml(`
-      <check-list>
+      <check-list default-checked="">
         <mock:shadow-root>
-          <slot></slot>
+          <div>
+            <calcite-label layout="inline">
+              <calcite-checkbox value="A"></calcite-checkbox>
+              A
+            </calcite-label>
+            <calcite-label layout="inline">
+              <calcite-checkbox value="B"></calcite-checkbox>
+              B
+            </calcite-label>
+            <calcite-label layout="inline">
+              <calcite-checkbox value="C"></calcite-checkbox>
+              C
+            </calcite-label>
+          </div>
         </mock:shadow-root>
       </check-list>
     `);
+    expect(page.root.defaultChecked).toEqual(true);
   });
 });
