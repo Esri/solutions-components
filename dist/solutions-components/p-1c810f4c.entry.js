7fd0f94c4324c106f877ae5c550cc30acc72bcec
Mike Tschudi <mtschudi@esri.com>
2023-04-27 17:36:35 -0700

    Remove dist

--- dist/solutions-components/p-1c810f4c.entry.js
+++
@@ -1,6 +0,0 @@
-/*!
- * Copyright 2022 Esri
- * Licensed under the Apache License, Version 2.0
- * http://www.apache.org/licenses/LICENSE-2.0
- */
-import{r as t,c as s,g as e}from"./p-f8be5d5f.js";const r=class{constructor(e){t(this,e),this.stateLoaded=s(this,"stateLoaded",7),this.value="",this.templates=[],this.authentication=void 0}connectedCallback(){this._initValueObserver()}render(){return null}_initValueObserver(){this._valueObserver=new MutationObserver((t=>{t.some((t=>{const s=t.target[t.attributeName];if("attributes"===t.type&&"value"===t.attributeName&&s!==t.oldValue&&""!==s)return!0}))})),this._valueObserver.observe(this.el,{attributes:!0,attributeOldValue:!0})}get el(){return e(this)}};export{r as store_manager}
--- dist/solutions-components/p-ce3e73ae.entry.js
+++
@@ -1,6 +0,0 @@
-/*!
- * Copyright 2022 Esri
- * Licensed under the Apache License, Version 2.0
- * http://www.apache.org/licenses/LICENSE-2.0
- */
-import{r as s,h as t,H as r,g as l}from"./p-f8be5d5f.js";const e=class{constructor(t){s(this,t)}render(){return t(r,null,t("slot",null))}async _getTranslations(){}get el(){return l(this)}};e.style=":host{display:block}";export{e as list_item}
