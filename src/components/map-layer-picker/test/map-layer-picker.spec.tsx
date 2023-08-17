bf30cb2d72172eba6071c12277da7a38bb972e71
Mike Tschudi <mtschudi@esri.com>
2023-08-17 08:05:41 -0700

    Add placeholder for map-layer-picker test

--- src/components/map-layer-picker/test/map-layer-picker.spec.tsx
+++ src/components/map-layer-picker/test/map-layer-picker.spec.tsx
@@ -14,6 +14,12 @@
  * limitations under the License.
  */
 
+xdescribe('map-layer-picker', () => {
+  it('is a placeholder', () => {
+  });
+});
+
+
 /*
 import { h } from '@stencil/core';
 import { newSpecPage } from '@stencil/core/testing';
