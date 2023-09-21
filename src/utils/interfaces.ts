5ed110289afe883e9eab3793b086c9d749d70503
John Hauck <jhauck@esri.com>
2023-09-21 15:07:36 -0600

    update IMapInfo

--- src/utils/interfaces.ts
+++ src/utils/interfaces.ts
@@ -450,7 +450,7 @@ export interface IMapInfo {
   filters?: any[]; // TODO generate an interface for this once we know how it will be passed in
   layerInfos?: ILayerInfo[];
   _hasValidLayers?: boolean;
-  visible: boolean;
+  visible?: boolean;
 }
 
 export interface ILayerInfo {
