49c9949863b909a3949fbb6ec9fd54829debd718
John Hauck <jhauck@esri.com>
2024-06-18 11:19:56 -0600

    update version

--- support/releaseToGitHub.ts
+++ support/releaseToGitHub.ts
@@ -1,10 +1,9 @@
 /* eslint-disable import/first */
 require('dotenv').config();
-
-const childProcess = require("child_process");
-const githubRelease = require("gh-release");
-const pify = require("pify");
-const rimraf = require("rimraf");
+import * as childProcess from "child_process";
+import * as githubRelease from "gh-release";
+import * as pify from "pify";
+import * as rimraf from "rimraf";
 
 const packageFileName = childProcess.execSync("npm pack", { encoding: "utf-8" }).trim();
 const packageScope = "esri-";
@@ -23,7 +22,7 @@ const options = {
   yes: true // skips prompt
 };
 
-pify(githubRelease)(options)
+pify.default(githubRelease)(options)
   .then(() => console.info("Released on GitHub!"))
   .catch((error) => console.error("Could not create GitHub release", error))
   .then(() => rimraf.sync(packageFileName));
